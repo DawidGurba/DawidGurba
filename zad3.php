552b8d335e8c852d2d57719915e82bcbe7baf8cc
DawidGurba <90690443+DawidGurba@users.noreply.github.com>
2022-09-13 08:14:47 +0200

    Add files via upload

---
+++ zad3.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+$liczba = 10;
+if($liczba == 0 ) echo 'Podana liczba jest rowna 0';
+else if ($liczba > 0) 
+{echo 'Podana liczba jest wieksza od 0';
+}
+else echo 'Podana liczba jest mniejsza od 0';
+    ?>
+
+ 
+</body>
+</html>
