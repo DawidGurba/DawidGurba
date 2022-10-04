caf801b5fec6e0ec64878f8d8746b7e912ec49df
DawidGurba <90690443+DawidGurba@users.noreply.github.com>
2022-10-04 04:32:19 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+$n = 100;
+for($i = 0;i<=200;$i+=2)
+$n += $i;
+
+echo $n
+    ?>
+</body>
+</html>
